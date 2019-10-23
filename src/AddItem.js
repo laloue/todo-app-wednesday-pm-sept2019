b914991fdf1e3926c98e1607a24a0bb36f62f09a
laloue <ireland.lala@gmail.com>
2019-10-23 21:07:40 +0100

    completed class

--- src/AddItem.js
+++ src/AddItem.js
@@ -1,6 +1,27 @@
 import React from "react";
 
+//controlled components
 class AddItem extends React.Component {
+  state ={
+    newItemText:""
+  }
+  //Function which update state must always live where the State lives
+  updateNewItemText= (event) => {
+    console.log(event.target.value)
+    //this function should update the state whenever someone types
+    this.setState({
+      newItemText:event.target.value
+    });
+  };
+
+  handleClick=(event) =>{
+    event.preventDefault();
+    this.props.addNewTaskFunc(this.state.newItemText);
+    this.setState({
+      newItemtext:""
+    });
+  }
+
   render() {
     // JSX
     return (
@@ -11,15 +32,18 @@ class AddItem extends React.Component {
             className="form-control"
             id="newItem"
             placeholder="Type an item here"
+            value={this.state.newItemText}
+            onChange={this.updateNewItemText}
+
           ></input>
         </div>
         {/* this is a comment in JSX */}
-        <button type="submit" className="btn btn-primary mb-2">
+        <button type="submit" className="btn btn-primary mb-2" onClick={this.handleClick} disabled  >
           Add to List
         </button>
       </form>
-    );
-  }
+    )
+  };
 }
 
 export default AddItem;
--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 import React, { Component } from "react";
+import uuid from "uuid/v4";
 import Header from "./Header";
 import ItemCount from "./ItemCount";
 import AddItem from "./AddItem";
 import Item from "./Item";
 import "./App.css";
 
+
 class App extends Component {
   state = {
     tasks: [
-      { test: "shopping", completed: true, date: "2019-10-23", id: 1 },
-      { test: "cleaning", completed: true, date: "2019-10-24", id: 2 },
-      { test: "sleeping", completed: false, date: "2019-10-28", id: 3 },
-      { test: "cooking", completed: true, date: "2019-10-23", id: 4 },
+      { test: "shopping", completed: true, date: "2019-10-23", id: uuid()},
+      { test: "cleaning", completed: true, date: "2019-10-24", id: uuid ()},
+      { test: "sleeping", completed: false, date: "2019-10-28", id: uuid() },
+      { test: "cooking", completed: true, date: "2019-10-23", id: uuid() },
     ]
   }
+
+
+  //this fundction should update the state with a new task
+addNewTask= taskText => {
+  const tasksCopy =this.state.tasks.slice();
+  const newTask= {
+    text: taskText,
+    completed:false,
+    date:"2019-10-23",
+    id: uuid()
+  };
+  tasksCopy.push(newTask)
+
+  this.setState({
+    tasks: tasksCopy
+  });
+};
+
+
+
   render() {
     const completedTasks = this.state.tasks.filter(task => {
       return task.completed;
@@ -31,10 +53,10 @@ console.log(incompleteTasks)
 return (
   <div className="container">
     <Header />
-    <AddItem />
+    <AddItem addNewTaskFunc={this.addNewTask}/>
     <ItemCount count={this.state.tasks.length} />
 
-    <h3> Tasks to Completed</h3>
+    <h3> Tasks Completed</h3>
     {completedTasks.map(task => {
     return <Item text={task.test} completed={task.completed} key={task.id}/>
     })}
