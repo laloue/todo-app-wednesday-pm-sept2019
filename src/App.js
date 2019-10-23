b60f41d13623dc48615ee5c0b483c3dc7e769cbd
laloue <ireland.lala@gmail.com>
2019-10-23 19:28:54 +0100

    State of Application

--- src/App.js
+++ src/App.js
@@ -5,16 +5,26 @@ import AddItem from "./AddItem";
 import Item from "./Item";
 import "./App.css";
 
-class App extends React.Component {
+class App extends Component {
+  state= {
+    tasks:[
+      {test: "shopping", completed: true, date:"2019-10-23", id: 1},
+      {test: "cleaning", completed: true, date:"2019-10-24", id:2},
+      {test: "sleeping", completed: false, date:"2019-10-28",id:3 },
+      {test: "cooking", completed: true, date:"2019-10-23", id:4},
+    ]
+  }
   render() {
     return (
       <div className="container">
         <Header />
         <AddItem />
-        <ItemCount count={6} />
-        <Item text="buy bin liners" completed={true} />
-        <Item text="walk the cat" completed={false} />
-        <Item text="brush the cat" quantity={5} completed={false} />
+        <ItemCount count={this.state.tasks.length} />
+        {this.state.tasks.map(task=>{
+          return <Item text={task.test} completed={task.completed}/>
+        })}
+
+      
       </div>
     );
   }
